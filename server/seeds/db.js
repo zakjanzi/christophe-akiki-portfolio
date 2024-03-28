c80c94518c9ff173f6d2b1259a9863f407ee7a4a
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2024-03-28 17:14:53 +0100

    db seed script update

--- server/seeds/db.js
+++ server/seeds/db.js
@@ -4,23 +4,21 @@ const Album = require("../Models/AlbumModel");
 
 connectToDB();
 
-const album = new Album();
-
-const albums = await album.collection.find({});
-
-if (albums.length > 0) {
-  return;
-}
-
-album.collection
-  .insertMany([
-    { name: "Automotive" },
-    { name: "Structures & Interiors" },
-    { name: "Extreme Sports" },
-    { name: "Portraits & Modeling" },
-    { name: "Concerts & Events" },
-  ])
+Album.find({})
+  .exec()
   .then((res) => {
-    console.log(res);
-    return;
+    if (res.length > 0) {
+      return;
+    }
+
+    Album.insertMany([
+      { name: "Automotive" },
+      { name: "Structures & Interiors" },
+      { name: "Extreme Sports" },
+      { name: "Portraits & Modeling" },
+      { name: "Concerts & Events" },
+    ]).then((res) => {
+      console.log(res);
+      return;
+    });
   });
